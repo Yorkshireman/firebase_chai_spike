37428c01e89618e6047a4e6c9fb7622788361846
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-10-17 23:19:49 +0400

    basic test for testing html working

--- test/test.js
+++ test/test.js
@@ -1,23 +1,26 @@
 var assert = require('chai').assert;
 var expect = require('chai').expect;
+test = require('selenium-webdriver/testing'),
+webdriver = require('selenium-webdriver');
 
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
-});
-
-describe('Random Test Tests', function() {
+describe('Writing Messages', function() {
   it("'foo' should be a string using 'assert'", function() {
 		var foo = "bar"
 		assert.typeOf(foo, 'string');
   });
 
-  it("'foo' should be a string using 'expect'", function() {
-  	var foo = "bar"
-  	expect(foo).to.be.a("string");
-  })
+  test.it('index.html should have Home Page displayed', function() {
+    var driver = new webdriver.Builder().
+    withCapabilities(webdriver.Capabilities.chrome()).
+    build();
+    driver.get('file:/home/andrew/projects/firebase_chai_spike/index.html');
+    
+    var title = driver.findElement(webdriver.By.id('title'));
+
+    title.getInnerHtml().then(function(html) {
+        expect(html).to.equal("Home Page");
+    });
+    
+    driver.quit();
+  });
 });
