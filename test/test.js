0b15c4bc9253a1a416e921de6fd684e43e82ae89
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-10-17 17:57:36 +0400

    setup chai assertions

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
-var assert = require('assert');
+var assert = require('chai').assert;
+var expect = require('chai').expect;
+
 describe('Array', function() {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
@@ -6,4 +8,16 @@ describe('Array', function() {
       assert.equal(-1, [1,2,3].indexOf(0));
     });
   });
+});
+
+describe('Random Test Tests', function() {
+  it("'foo' should be a string using 'assert'", function() {
+		var foo = "bar"
+		assert.typeOf(foo, 'string');
+  });
+
+  it("'foo' should be a string using 'expect'", function() {
+  	var foo = "bar"
+  	expect(foo).to.be.a("string");
+  })
 });
