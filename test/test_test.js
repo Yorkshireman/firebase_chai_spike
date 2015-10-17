1d1e6e06fa476453602880065b70ffa9e6729deb
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-10-17 22:23:12 +0400

    test test with selenium web-driver and mocha passing

--- test/test_test.js
+++ test/test_test.js
@@ -1,10 +1,19 @@
-var webdriver = require('selenium-webdriver');
-
-var driver = new webdriver.Builder().
-   withCapabilities(webdriver.Capabilities.chrome()).
-   build();
+var assert = require('assert'),
+test = require('selenium-webdriver/testing'),
+webdriver = require('selenium-webdriver');
  
-driver.get('http://www.google.com');
-driver.findElement(webdriver.By.name('q')).sendKeys('simple programmer');
-driver.findElement(webdriver.By.name('btnG')).click();
-driver.quit();
+test.describe('Google Search', function() {
+  this.timeout(15000);
+  test.it('should work', function() {
+    var driver = new webdriver.Builder().
+    withCapabilities(webdriver.Capabilities.chrome()).
+    build();
+driver.get('http://www.google.co.uk');
+    var searchBox = driver.findElement(webdriver.By.name('q'));
+    searchBox.sendKeys('simple programmer');
+    searchBox.getAttribute('value').then(function(value) {
+      assert.equal(value, 'simple programmer');
+    });
+    driver.quit();
+  });
+});
